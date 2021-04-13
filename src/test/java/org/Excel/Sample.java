a6c24533bc570e4bf8f730c4fabefa7997c9333b
venkateshlr56 <venkateshlr56@gmail.com>
2021-04-13 15:07:54 +0530

    pushing my code

---
+++ src/test/java/org/Excel/Sample.java
@@ -0,0 +1,68 @@
+package org.Excel;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Sample {
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		//File location
+		File f=new File("C:\\Users\\Nishanthi\\eclipse-workspace\\FirstMaven\\Excel\\Sample_Excel.xlsx");
+		
+		//To get into the file
+		FileInputStream fin=new FileInputStream(f);
+		
+		//To get into the workbook
+		Workbook w= new XSSFWorkbook(fin);
+		
+		//To get into the sheet
+		Sheet sheet = w.getSheet("Sheet1");
+		
+//		Row row = sheet.getRow(0);
+		
+		//To get the number of rows present
+		int physicalNumberOfRows = sheet.getPhysicalNumberOfRows();
+		System.out.println("No of rows:"+physicalNumberOfRows);
+		
+		//To print all the values in the sheet
+		for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+			Row row2 = sheet.getRow(i);
+			
+			for (int j = 0; j < row2.getPhysicalNumberOfCells(); j++) {
+				Cell cell = row2.getCell(j);
+				System.out.println(cell);
+			}
+			
+		}
+		
+		//To get the number of cells present
+//		int physicalNumberOfCells = row.getPhysicalNumberOfCells();
+//		System.out.println("No of cells:"+physicalNumberOfCells);
+		
+		
+		
+		
+		
+//		//To get into the row
+//		Row row = sheet.getRow(1);
+//		
+//		//To get into the cell
+//		Cell cell = row.getCell(0);
+//		
+//		//To print the data
+//		System.out.println(cell);
+		
+	}
+
+}
